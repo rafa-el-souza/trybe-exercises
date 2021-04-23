6dbc6dea747a66e9be26f8637363ff84909d4c00
rafa-el-souza <rafaelmvsouza@gmail.com>
2021-04-23 16:06:21 -0300

    Completed exercise 4 | comparedTo20.js

---
+++ modulo_1/bloco_4/introducao-a-javascript-e-logica-de-programacao_2/comparedTo20.js
@@ -0,0 +1,19 @@
+// Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sum = 0;
+let mean = 0;
+
+for (number of numbers) {
+    sum += number;
+}
+
+mean = sum / numbers.length;
+
+console.log(mean);
+
+if (mean > 20) {
+    console.log("valor maior que 20");
+}   else {
+    console.log("valor menor ou igual a 20")
+}
