3418d0c96332bdd909335e7262a1b157abfd0f7e
rafa-el-souza <rafaelmvsouza@gmail.com>
2021-04-23 16:01:51 -0300

    Completed exercise 3 | arrayMeanAndPrint.js

---
+++ modulo_1/bloco_4/introducao-a-javascript-e-logica-de-programacao_2/arrayMeanAndPrint.js
@@ -0,0 +1,13 @@
+// Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sum = 0;
+let mean = 0;
+
+for (number of numbers) {
+    sum += number;
+}
+
+mean = sum / numbers.length;
+
+console.log(mean);
