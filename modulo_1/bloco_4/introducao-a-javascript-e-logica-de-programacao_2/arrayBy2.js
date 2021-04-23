18b134e6545e81be071360673793d29cd441668b
rafa-el-souza <rafaelmvsouza@gmail.com>
2021-04-23 19:10:51 -0300

    Completed exercise 9 | arrayBy2.js

---
+++ modulo_1/bloco_4/introducao-a-javascript-e-logica-de-programacao_2/arrayBy2.js
@@ -0,0 +1,9 @@
+// Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2 .
+
+let array = [];
+
+for (let index = 0; index < 25; index += 1) {
+    array.push((index + 1) / 2);
+}
+
+console.log(array);
