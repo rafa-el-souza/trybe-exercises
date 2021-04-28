37f96817f5d9431c730770d976b5fdec89a593b8
rafa-el-souza <rafaelmvsouza@gmail.com>
2021-04-28 10:13:21 -0300

    Exercise 8 | indexOfLowerValueFunction.js | Completed exercise

--- modulo_1/bloco_4/introducao-a-javascript-e-logica-de-programacao_4/indexOfHighestValueFunction.js
+++ modulo_1/bloco_4/introducao-a-javascript-e-logica-de-programacao_4/indexOfHighestValueFunction.js
@@ -1,12 +1,12 @@
 // Crie uma função que receba um array de inteiros e retorne o índice do maior valor.
 
-let test = [2, 3, 6, 7, 10, 1];
+let test = [2, 3, 6, 7, 10, 1, 20];
 
 function indexOfHighestValue(array) {
     let highestValue = undefined;
-    let highestValueIndex = undefined;
-    for (let index = 0; index < array.length; index += 1) {
-        if (array[index] > array[index + 1]) {
+    let highestValueIndex = 0;
+    for (let index = 1; index < array.length; index += 1) {
+        if (array[highestValueIndex] < array[index]) {
             highestValue = array[index];
             highestValueIndex = index;
         }
