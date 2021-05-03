f16e4adea12557542fbc961833bf1d271dc73ceb
rafa-el-souza <rafaelmvsouza@gmail.com>
2021-05-03 18:29:08 -0300

    Completed exercise 1 | monthBuilder()

--- modulo_1/bloco_5/javascript-dom-eventos-e-web-storage_3/script.js
+++ modulo_1/bloco_5/javascript-dom-eventos-e-web-storage_3/script.js
@@ -15,4 +15,30 @@ function createDaysOfTheWeek() {
   
   // Escreva seu código abaixo.
 
-  
+  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+  const holidays = [24, 25, 31];
+  const fridays = [4, 11, 18, 25];
+
+  const ulDays = document.querySelector('#days');
+
+  function monthBuilder() {
+    for (let index = 0; index < dezDaysList.length; index += 1) {
+        let li = document.createElement('li');
+        li.className = 'day';
+        for (let index2 = 0; index2 < holidays.length; index2 += 1) {
+            if (holidays[index2] === dezDaysList[index]) {
+                li.classList.add('holiday');
+            }
+        }
+        for (let index3 = 0; index3 < fridays.length; index3 += 1) {
+            if (fridays[index3] === dezDaysList[index]) {
+                li.classList.add('friday');
+                console.log(dezDaysList[index]);
+            }
+        }
+        li.innerHTML = dezDaysList[index];
+        ulDays.appendChild(li);
+    }
+  }
+
+  monthBuilder();
