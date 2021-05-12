6f91d50e80e5491ce8aa5a5e858bc967b6009142
rafa-el-souza <rafaelmvsouza@gmail.com>
2021-05-12 08:46:26 -0300

    Exercicio 2 | Adiciona campo de dados do ultimo emprego | Adiciona verificacao de data por expressao regular

--- modulo_1/bloco_6/html-e-css-forms-flexbox-e-responsivo_1/script.js
+++ modulo_1/bloco_6/html-e-css-forms-flexbox-e-responsivo_1/script.js
@@ -57,6 +57,7 @@ const statesInitials = [
     'TO',
 ]
 const stateInput = document.querySelector('#state-input');
+const startDateInput = document.querySelector('#start-date-input');
 
 for (let index = 0; index < states.length; index += 1) {
     const option = document.createElement('option');
@@ -64,3 +65,16 @@ for (let index = 0; index < states.length; index += 1) {
     option.value = statesInitials[index];
     stateInput.appendChild(option);
 }
+
+startDateInput.addEventListener('change', () => {
+    const regexDate = /^(0[1-9]|1\d|2\d|3[01])\/(0[1-9]|1[012])\/(1\d\d\d|2\d\d\d)$/;
+    const startDateString = startDateInput.value;
+    if (regexDate.test(startDateString)) {
+        console.log(startDateString);
+        return startDateString;
+    } else {
+        console.log('Formato de data inválido');
+        alert('Formato de data inválido');
+        return 'Formato inválido';
+    }
+});
