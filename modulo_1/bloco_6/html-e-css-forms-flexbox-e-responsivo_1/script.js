8c23fcf9ad9b3653826f6fe523360e75d92b6200
rafa-el-souza <rafaelmvsouza@gmail.com>
2021-05-12 08:43:33 -0300

    Exercicio 1 | Adiciona campo de dados pessoais

---
+++ modulo_1/bloco_6/html-e-css-forms-flexbox-e-responsivo_1/script.js
@@ -0,0 +1,66 @@
+const states = [
+    'Acre',
+    'Alagoas',
+    'Amapá',
+    'Amazonas',
+    'Bahia',
+    'Ceará',
+    'Distrito Federal',
+    'Espirito Santo',
+    'Goiás',
+    'Maranhão',
+    'Mato Grosso do Sul',
+    'Mato Grosso',
+    'Minas Gerais',
+    'Pará',
+    'Paraíba',
+    'Paraná',
+    'Pernambuco',
+    'Piauí',
+    'Rio de Janeiro',
+    'Rio Grande do Norte',
+    'Rio Grande do Sul',
+    'Rondônia',
+    'Roraima',
+    'Santa Catarina',
+    'São Paulo',
+    'Sergipe',
+    'Tocantins'
+];
+const statesInitials = [
+    'AC',
+    'AL',
+    'AP',
+    'AM',
+    'BA',
+    'CE',
+    'DF',
+    'ES',
+    'GO',
+    'MA',
+    'MS',
+    'MT',
+    'MG',
+    'PA',
+    'PB',
+    'PR',
+    'PE',
+    'PI',
+    'RJ',
+    'RN',
+    'RS',
+    'RO',
+    'RR',
+    'SC',
+    'SP',
+    'SE',
+    'TO',
+]
+const stateInput = document.querySelector('#state-input');
+
+for (let index = 0; index < states.length; index += 1) {
+    const option = document.createElement('option');
+    option.innerHTML = states[index];
+    option.value = statesInitials[index];
+    stateInput.appendChild(option);
+}
