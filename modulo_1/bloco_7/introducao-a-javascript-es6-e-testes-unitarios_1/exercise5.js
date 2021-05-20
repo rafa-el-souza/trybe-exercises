2d409f968242a65de0c26db5677db319f1466cf9
rafa-el-souza <rafaelmvsouza@gmail.com>
2021-05-20 13:28:55 -0300

    Exercicio 5 | Resolvido

--- modulo_1/bloco_7/introducao-a-javascript-es6-e-testes-unitarios_1/exercise5.js
+++ modulo_1/bloco_7/introducao-a-javascript-es6-e-testes-unitarios_1/exercise5.js
@@ -2,3 +2,10 @@
 //  - Um botão ao qual será associado um event listener ;
 //  - Uma variável clickCount no arquivo JavaScript que acumule o número de clicks no botão;
 //  - Um campo no HTML que vá atualizando a quantidade de clicks no botão conforme a variável clickCount é atualizada.
+const countMeButton = document.querySelector('#button');
+const counterDisplay = document.querySelector('#counter-display');
+let counter = 0;
+countMeButton.addEventListener('click', () => {
+    counter += 1;
+    counterDisplay.innerHTML = counter;
+});
