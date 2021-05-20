9ae90414c734bcb8205371d015b68fbed0a215df
rafa-el-souza <rafaelmvsouza@gmail.com>
2021-05-20 11:28:32 -0300

    Exercicio 2 | Resolvido 1.1 | array.sort(funcao de comparaçao opcional)

--- modulo_1/bloco_7/introducao-a-javascript-es6-e-testes-unitarios_1/exercise2.js
+++ modulo_1/bloco_7/introducao-a-javascript-es6-e-testes-unitarios_1/exercise2.js
@@ -1,10 +1,11 @@
-// Copie o código abaixo e faça uma função que retorne o array oddsAndEvens em ordem crescente.
-//  - Utilize template literals para que a chamada console.log(oddsAndEvens); retorne "Os números 2,3,4,7,10,13 se encontram ordenados de forma crescente!".
-//  - Bônus (opcional): tente fazer o mesmo exercício utilizando o método array.sort() . 
+//  1.Faça uma função que retorne o array oddsAndEvens em ordem crescente.
+//    - Bônus (opcional): tente fazer o mesmo exercício utilizando o método array.sort() .
+//  2.Utilize template literals para que a chamada console.log(oddsAndEvens); retorne "Os números 2,3,4,7,10,13 se encontram ordenados de forma crescente!".
+//   
 //  Spoiler: É possível realizar uma função que ordene qualquer array de números.
 
 const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
-// Seu código aqui.
+oddsAndEvens.sort((a, b) => a-b);
 
 console.log(oddsAndEvens);
