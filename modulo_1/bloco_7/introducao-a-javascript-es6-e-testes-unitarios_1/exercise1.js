3137cbaff70e5e438924b5e1fa512dd87c181b09
rafa-el-souza <rafaelmvsouza@gmail.com>
2021-05-20 11:11:14 -0300

    Exercicio 1 | Resolvido 1.3 | Conversao para uso de template literals

--- modulo_1/bloco_7/introducao-a-javascript-es6-e-testes-unitarios_1/exercise1.js
+++ modulo_1/bloco_7/introducao-a-javascript-es6-e-testes-unitarios_1/exercise1.js
@@ -4,11 +4,11 @@
 
 const testingScope = (escopo) => {
     if (escopo === true) {
-      let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-      ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+      let ifScope = `Não devo ser utilizada fora do meu escopo (if)`;
+      ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
       console.log(ifScope);
     } else {
-      const elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+      const elseScope = `Não devo ser utilizada fora meu escopo (else)`;
       console.log(elseScope);
     }
   }
