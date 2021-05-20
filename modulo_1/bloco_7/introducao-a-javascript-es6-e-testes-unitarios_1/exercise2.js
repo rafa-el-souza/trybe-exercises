2efcf39411c725bd689b707f3fd85659231c2c00
rafa-el-souza <rafaelmvsouza@gmail.com>
2021-05-20 11:14:02 -0300

    Exercicio 2 | Enunciado

---
+++ modulo_1/bloco_7/introducao-a-javascript-es6-e-testes-unitarios_1/exercise2.js
@@ -0,0 +1,10 @@
+// Copie o código abaixo e faça uma função que retorne o array oddsAndEvens em ordem crescente.
+//  - Utilize template literals para que a chamada console.log(oddsAndEvens); retorne "Os números 2,3,4,7,10,13 se encontram ordenados de forma crescente!".
+//  - Bônus (opcional): tente fazer o mesmo exercício utilizando o método array.sort() . 
+//  Spoiler: É possível realizar uma função que ordene qualquer array de números.
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+// Seu código aqui.
+
+console.log(oddsAndEvens);
