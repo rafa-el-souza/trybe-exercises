bbbbb275b11f084089c278f782c2a39b00aad94f
rafa-el-souza <rafaelmvsouza@gmail.com>
2021-05-20 22:03:45 -0300

    Exercicios 1 e 2 | objects.js | Resolvidos

--- modulo_1/bloco_7/introducao-a-javascript-es6-e-testes-unitarios_2/objects.js
+++ modulo_1/bloco_7/introducao-a-javascript-es6-e-testes-unitarios_2/objects.js
@@ -7,51 +7,49 @@
 */
 
 const order = {
-    name: 'Rafael Andrade',
-    phoneNumber: '11-98763-1416',
-    address: {
-      street: 'Rua das Flores',
-      number: '389',
-      apartment: '701',
+name: 'Rafael Andrade',
+phoneNumber: '11-98763-1416',
+address: {
+    street: 'Rua das Flores',
+    number: '389',
+    apartment: '701',
+},
+order: {
+    pizza: {
+    margherita: {
+        amount: 1,
+        price: 25,
     },
-    order: {
-      pizza: {
-        margherita: {
-          amount: 1,
-          price: 25,
-        },
-        pepperoni: {
-          amount: 1,
-          price: 20,
-        }
-      },
-      drinks: {
-        coke: {
-          type: 'Coca-Cola Zero',
-          price: 10,
-          amount: 1,
-        }
-      },
-      delivery: {
-        deliveryPerson: 'Ana Silveira',
-        price: 5,
-      }
+    pepperoni: {
+        amount: 1,
+        price: 20,
+    }
     },
-    payment: {
-      total: 60,
+    drinks: {
+    coke: {
+        type: 'Coca-Cola Zero',
+        price: 10,
+        amount: 1,
+    }
     },
-  };
-  
-  const customerInfo = (order) => {
-    // Adicione abaixo as informações necessárias.
-  
-  }
-  
-  customerInfo(order);
-  
-  const orderModifier = (order) => {
-    // Adicione abaixo as informações necessárias.
-  
-  }
-  
-  orderModifier(order);
+    delivery: {
+    deliveryPerson: 'Ana Silveira',
+    price: 5,
+    }
+},
+payment: {
+    total: 60,
+},
+};
+
+const customerInfo = (order) => `Olá, ${order.order.delivery.deliveryPerson}, entrega para ${order.name}, Telefone: ${order.phoneNumber}, ${order.address.street}, N: ${order.address.number}, AP: ${order.address.apartment}.`;
+
+console.log(customerInfo(order));
+
+const orderModifier = (order) => {
+order.name = 'Luiz Silva';
+order.payment.total = 50;
+return `Olá, ${order.name}, o total do seu pedido de ${Object.keys(order.order.pizza)} e ${order.order.drinks.coke.type} é R$ ${order.payment.total},00.`;
+};
+
+console.log(orderModifier(order));
