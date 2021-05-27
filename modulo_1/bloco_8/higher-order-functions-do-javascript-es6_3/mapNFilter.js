4bc717df8d952240e8729c13cd5c33501ea7c2b0
rafa-el-souza <rafaelmvsouza@gmail.com>
2021-05-27 19:18:12 -0300

    Resolve exercicio 4 | mapNFilter.js

--- modulo_1/bloco_8/higher-order-functions-do-javascript-es6_3/mapNFilter.js
+++ modulo_1/bloco_8/higher-order-functions-do-javascript-es6_3/mapNFilter.js
@@ -115,12 +115,14 @@ const expectedResult2 = [
 
 function nameAndAge() {
   // escreva seu código aqui
-  return books.map((book) => {
-    return {
-      age: (book.releaseYear - book.author.birthYear),
-      author: book.author.name,
-    };
-  }).sort((a, b) => a.age - b.age);
+  return books
+    .map((book) => {
+      return {
+        age: book.releaseYear - book.author.birthYear,
+        author: book.author.name,
+      };
+    })
+    .sort((a, b) => a.age - b.age);
 }
 
 assert.deepStrictEqual(nameAndAge(), expectedResult2);
@@ -129,39 +131,78 @@ assert.deepStrictEqual(nameAndAge(), expectedResult2);
 // Dica: use a função filter ;
 
 const expectedResult3 = [
-  { 
+  {
     id: 1,
     name: 'As Crônicas de Gelo e Fogo',
     genre: 'Fantasia',
     author: { name: 'George R. R. Martin', birthYear: 1948 },
-    releaseYear: 1991
+    releaseYear: 1991,
   },
   {
     id: 2,
     name: 'O Senhor dos Anéis',
     genre: 'Fantasia',
     author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
-    releaseYear: 1954
+    releaseYear: 1954,
   },
   {
     id: 3,
     name: 'Fundação',
     genre: 'Ficção Científica',
     author: { name: 'Isaac Asimov', birthYear: 1920 },
-    releaseYear: 1951
+    releaseYear: 1951,
   },
   {
     id: 4,
     name: 'Duna',
     genre: 'Ficção Científica',
     author: { name: 'Frank Herbert', birthYear: 1920 },
-    releaseYear: 1965
-  }
+    releaseYear: 1965,
+  },
 ];
 
 function fantasyOrScienceFiction() {
   // escreva seu código aqui
-  return books.filter((book) => (book.genre === 'Ficção Científica' || book.genre === 'Fantasia'));
+  return books.filter(
+    (book) => book.genre === 'Ficção Científica' || book.genre === 'Fantasia'
+  );
 }
 
 assert.deepStrictEqual(fantasyOrScienceFiction(), expectedResult3);
+
+// 4 - Crie um array ordenado pelos livros com mais de 60 anos de publicação e ordene-o pelo livro mais velho.
+// Dica: use as funções filter e sort
+
+const expectedResult4 = [
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: { name: 'H. P. Lovecraft', birthYear: 1890 },
+    releaseYear: 1928,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: { name: 'Isaac Asimov', birthYear: 1920 },
+    releaseYear: 1951,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
+    releaseYear: 1954,
+  },
+];
+
+function oldBooksOrdered() {
+  // escreva seu código aqui
+  const currentYear = 2021;
+  return books
+    .filter((book) => currentYear - book.releaseYear > 60)
+    .sort((a, b) => a.releaseYear - b.releaseYear);
+}
+
+assert.deepStrictEqual(oldBooksOrdered(), expectedResult4);
