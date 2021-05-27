6d06f881625c844f3980fda76a9d04c9e9b2df6f
rafa-el-souza <rafaelmvsouza@gmail.com>
2021-05-27 11:48:24 -0300

    Resolve exercicio 1.3 | exercise1.js

--- modulo_1/bloco_8/higher-order-functions-do-javascript-es6_2/exercise1.js
+++ modulo_1/bloco_8/higher-order-functions-do-javascript-es6_2/exercise1.js
@@ -101,3 +101,23 @@ function smallerName2() {
 }
 
 assert.strictEqual(smallerName2(), 'Duna');
+
+// 3 - Encontre o primeiro livro cujo nome possui 26 caracteres.
+
+const expectedResult = {
+  author: {
+    birthYear: 1948,
+    name: 'George R. R. Martin',
+  },
+  genre: 'Fantasia',
+  id: 1,
+  name: 'As Crônicas de Gelo e Fogo',
+  releaseYear: 1991,
+};
+
+function getNamedBook() {
+  // escreva seu código aqui
+  return books.find((book) => book.name.length === 26);
+}
+
+assert.deepStrictEqual(getNamedBook(), expectedResult);
