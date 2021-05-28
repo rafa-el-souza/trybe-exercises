f76c1279922c8f3ce6ecd1c7ce3c9b9c914b841c
rafa-el-souza <rafaelmvsouza@gmail.com>
2021-05-28 16:09:31 -0300

    Exercicio 5 | Resolvido

--- modulo_1/bloco_8/higher-order-functions-do-javascript-es6_4/reduce.js
+++ modulo_1/bloco_8/higher-order-functions-do-javascript-es6_4/reduce.js
@@ -116,3 +116,26 @@ function longestNamedBook() {
 }
 
 assert.deepStrictEqual(longestNamedBook(), expectedResult4);
+
+
+// 5 - Dada a array de nomes, retorne a quantidade de vezes em que aparecem a letra a maiúscula ou minúscula.
+
+const names = [
+  'Aanemarie',
+  'Adervandes',
+  'Akifusa',
+  'Abegildo',
+  'Adicellia',
+  'Aladonata',
+  'Abeladerco',
+  'Adieidy',
+  'Alarucha',
+];
+
+function containsA() {
+  // escreva seu código aqui
+  return names.reduce((acc, name) => acc + name.match(/a|A/g).length, 0);
+}
+
+assert.deepStrictEqual(containsA(), 20);
+
